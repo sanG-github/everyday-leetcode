2b4ba684dee00071ebb563515f44f346b2808188
Sang Huynh Thanh <63148598+sanG-github@users.noreply.github.com>
2026-01-07 08:10:12 +0700

    Time: 87 ms (100%), Space: 212.1 MB (100%) - LeetHub

--- 2601-prime-subtraction-operation/2601-prime-subtraction-operation.rb
+++ 2601-prime-subtraction-operation/2601-prime-subtraction-operation.rb
@@ -1,21 +1,36 @@
-require 'prime'
+# @param {Integer[]} nums
+# @return {Boolean}
+class Integer < Numeric
+    def is_prime?
+        return true if self == 0
+        return false if self <= 1
+        2.upto(Math.sqrt(self).to_i) do |x|
+            return false if self%x == 0
+        end 
+        true
+    end
+
+    def previous_prime
+        n = self - 1
+        
+        return 0 if n <= 0
+
+        n = n - 1 until n.is_prime?
+
+        n
+    end
+end
 
 def prime_sub_operation(nums)
   prev = 0
 
   nums.each do |num|
-    best = num
+    prime = (num - prev).previous_prime
 
-    Prime.each(num - 1) do |p|
-      break if num <= p
-      
-      val = num - p
+    puts "checking prime: #{prime}, num: #{num}, prev: #{prev}"
+    return false if num - prime <= prev
 
-      best = [best, val].min if val > prev
-    end
-
-    return false if best <= prev
-    prev = best
+    prev = num - prime
   end
 
   true
