38eb402958edeffb4ed0253f55bd3f4eaf7d0be9
Sang Huynh Thanh <63148598+sanG-github@users.noreply.github.com>
2026-01-11 17:13:46 +0700

    Time: 0 ms (100%), Space: 211.1 MB (100%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.rb
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.rb
@@ -1,44 +1,16 @@
 # @param {Integer} n
 # @return {Integer}
 def num_trees(n)
-    @hash = {}
-    # n = 1 -> 1
+  memo = Array.new(n + 1)
 
-    # n = 2 -> 2
-    # 1 - 2
-    # 2 - 1
+  dfs = lambda do |k|
+    return 1 if k <= 1
+    return memo[k] if memo[k]
 
-    # n = 3 -> 5
-    # pick 1 ([],[2,3]) -> (2)
-    # pick 2 ([1],[3]) -> (1)
-    # pick 3 ([1,2],[]) -> (2)
-
-    # n = 4 -> 14
-    # pick 1 ([],[2,3,4]) -> 5
-    # pick 2 ([1],[3,4]) -> 2
-    # pick 3 ([1,2],[4]) -> 2
-    # pick 4 ([1,2,3],[]) -> 5
-
-    # n = 5 ->
-    # pick 1 ([], [2,3,4,5]) -> 14
-    # pick 2 ([1], [3,4,5]) -> 5
-    # pick 3 ([1,2], [4,5]) -> 4
-    # pick 4 ([1,2,3], [5]) -> 5
-    # pick 5 ([1,2,3,4], []) -> 14
-
-    recursive(n)
-end
-
-def recursive(n)
-    return 1 if n == 0
-    return 1 if n == 1
-
-    return @hash[n] if @hash[n]
-
-    @hash[n] = n.times.reduce(0) do |acc, i|
-        left = i
-        right = n - (i+1)
-
-        acc + recursive(left) * recursive(right)
+    memo[k] = (0...k).reduce(0) do |sum, i|
+      sum + dfs.call(i) * dfs.call(k - i - 1)
     end
+  end
+
+  dfs.call(n)
 end
