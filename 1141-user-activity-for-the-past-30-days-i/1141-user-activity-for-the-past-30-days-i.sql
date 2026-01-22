5735039f1ed932a78c5825291db7e3b4480ae526
Sang Huynh Thanh <63148598+sanG-github@users.noreply.github.com>
2026-01-22 21:41:54 +0700

    Time: 430 ms (91.57%), Space: 0B (100%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -1,8 +1,4 @@
-SELECT 
-    activity_date AS day, 
-    COUNT(DISTINCT user_id) AS active_users
-FROM 
-    Activity
-WHERE 
-    DATEDIFF('2019-07-27', activity_date) < 30 AND DATEDIFF('2019-07-27', activity_date)>=0
-GROUP BY 1
+SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE (activity_date > "2019-06-27" AND activity_date <= "2019-07-27")
+GROUP BY activity_date;
