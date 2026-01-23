6db7f63f726c68a1739e4fe3a8cd0a90e9b959ff
Sang Huynh Thanh <63148598+sanG-github@users.noreply.github.com>
2026-01-23 10:12:48 +0700

    Time: 0 ms (100%), Space: 211.4 MB (73.33%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.rb
+++ 0520-detect-capital/0520-detect-capital.rb
@@ -1,15 +1,17 @@
 # @param {String} word
 # @return {Boolean}
 def detect_capital_use(word)
-    is_upcase = word[0] == word[0].upcase
+    word == word.capitalize || word == word.upcase || word == word.downcase
 
-    if is_upcase && word[1]
-        is_upcase = word[1] == word[1].upcase
-    end
+    # is_upcase = word[0] == word[0].upcase
+
+    # if is_upcase && word[1]
+    #     is_upcase = word[1] == word[1].upcase
+    # end
     
-    word.chars[1..-1].each do |char|
-        return false if (is_upcase && char != char.upcase) || (!is_upcase && char == char.upcase)
-    end
+    # word.chars[1..-1].each do |char|
+    #     return false if (is_upcase && char != char.upcase) || (!is_upcase && char == char.upcase)
+    # end
 
-    true
+    # true
 end
