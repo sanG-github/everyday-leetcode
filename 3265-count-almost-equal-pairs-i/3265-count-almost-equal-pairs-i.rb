afd49fa1001c7c0cf5f53e567c02b062d7dfc526
Sang Huynh Thanh <63148598+sanG-github@users.noreply.github.com>
2026-01-28 17:12:01 +0700

    Time: 349 ms (100%), Space: 211.9 MB (100%) - LeetHub

---
+++ 3265-count-almost-equal-pairs-i/3265-count-almost-equal-pairs-i.rb
@@ -0,0 +1,36 @@
+def count_pairs(nums)
+  digits = nums.map { |n| n.digits }
+  count = 0
+
+  (0...nums.size).each do |i|
+    (i + 1...nums.size).each do |j|
+      a, b = normalize(digits[i], digits[j])
+      count += 1 if almost_equal?(a, b)
+    end
+  end
+
+  count
+end
+
+def normalize(a, b)
+  max = [a.size, b.size].max
+  [a + [0] * (max - a.size),
+   b + [0] * (max - b.size)]
+end
+
+def almost_equal?(a, b)
+  diff = []
+
+  a.each_index do |i|
+    if a[i] != b[i]
+      diff << i
+      return false if diff.size > 2
+    end
+  end
+
+  return true if diff.empty?
+  return false unless diff.size == 2
+
+  i, j = diff
+  a[i] == b[j] && a[j] == b[i]
+end
