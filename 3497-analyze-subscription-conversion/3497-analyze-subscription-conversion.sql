00825fbd07946c1986a5caa2090de6130643fe19
Sang Huynh Thanh <63148598+sanG-github@users.noreply.github.com>
2026-01-31 17:34:13 +0700

    Time: 303 ms (50.69%), Space: 0B (100%) - LeetHub

--- 3497-analyze-subscription-conversion/3497-analyze-subscription-conversion.sql
+++ 3497-analyze-subscription-conversion/3497-analyze-subscription-conversion.sql
@@ -4,9 +4,5 @@ SELECT
     ROUND(AVG(CASE WHEN activity_type = 'free_trial' THEN activity_duration ELSE NULL END), 2) as trial_avg_duration,
     ROUND(AVG(CASE WHEN activity_type = 'paid' THEN activity_duration ELSE NULL END), 2) as paid_avg_duration
 FROM UserActivity
-WHERE user_id IN (
-    SELECT DISTINCT user_id
-    FROM UserActivity
-    WHERE activity_type = 'paid'
-)
 GROUP BY user_id
+HAVING SUM(activity_type = 'paid') > 0;
