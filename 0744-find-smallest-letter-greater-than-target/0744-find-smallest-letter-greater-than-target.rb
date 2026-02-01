063bf8c2fb06359da4d2e36ce32c4cda7d7910c6
Sang Huynh Thanh <63148598+sanG-github@users.noreply.github.com>
2026-02-01 10:21:24 +0700

    Time: 0 ms (100%), Space: 212.2 MB (10%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.rb
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.rb
@@ -1,18 +1,26 @@
+# def next_greatest_letter(letters, target)
+#   l = 0
+#   r = letters.size
+
+#   while l < r
+#     m = (l + r) / 2
+#     puts "l=#{l}, m=#{m}, r=#{r}"
+
+#     if letters[m] <= target
+#       l = m + 1
+#     else
+#       r = m
+#     end
+#   end
+
+#   # wrap-around
+#   letters[l % letters.size]
+# end
+
 def next_greatest_letter(letters, target)
-  l = 0
-  r = letters.size
-
-  while l < r
-    m = (l + r) / 2
-    puts "l=#{l}, m=#{m}, r=#{r}"
-
-    if letters[m] <= target
-      l = m + 1
-    else
-      r = m
+    letters.each do |letter|
+        return letter if letter > target
     end
-  end
 
-  # wrap-around
-  letters[l % letters.size]
+    letters[0]
 end
