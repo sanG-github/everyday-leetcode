72c243f8d16712a18db6ef86c246880d7c2fbb42
Sang Huynh Thanh <63148598+sanG-github@users.noreply.github.com>
2026-02-03 15:38:41 +0700

    Time: 8 ms (66.67%), Space: 211.5 MB (100%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.rb
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.rb
@@ -1,21 +1,20 @@
 # @param {String} s
 # @return {Integer}
 def maximum_length_substring(s)
-  freq = Hash.new(0)
-  left = 0
-  max_len = 0
+    freq = Hash.new(0)
+    left = 0
+    longest = 0
 
-  s.each_char.with_index do |ch, right|
-    freq[ch] += 1
+    s.each_char.with_index do |char, r|
+        freq[char] += 1
 
-    # shrink window if a character appears more than twice
-    while freq[ch] > 2
-      freq[s[left]] -= 1
-      left += 1
+        while freq[char] > 2 # shrink the range by moving left
+            freq[s[left]] -= 1
+            left += 1
+        end
+
+        longest = [longest, r - left + 1].max
     end
 
-    max_len = [max_len, right - left + 1].max
-  end
-
-  max_len
+    longest
 end
