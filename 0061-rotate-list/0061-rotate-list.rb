a1acd25090d77074962e0a6f7603601b76612f4c
Sang Huynh Thanh <63148598+sanG-github@users.noreply.github.com>
2026-02-04 15:21:52 +0700

    Time: 2 ms (20.83%), Space: 211.3 MB (100%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.rb
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} head
+# @param {Integer} k
+# @return {ListNode}
+def rotate_right(head, k)
+    return unless head 
+
+    runner = head
+    count = 1
+
+    while runner.next
+        runner = runner.next
+        count += 1
+    end
+
+    tail = runner
+    runner = head
+    k = k % count
+
+    return head if k == 0
+
+    puts "tail: #{tail.val}, count: #{count}, k: #{k}"
+
+    while count - k - 1 > 0
+        runner = runner.next
+        count -= 1
+    end
+
+    new_head = runner.next
+    runner.next = nil
+    tail.next = head
+
+    new_head
+end
