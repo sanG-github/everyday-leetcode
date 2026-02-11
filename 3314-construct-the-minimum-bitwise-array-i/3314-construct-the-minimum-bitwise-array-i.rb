58d760d5f4d86643ecd889cc8729690b152223d4
Sang Huynh Thanh <63148598+sanG-github@users.noreply.github.com>
2026-02-11 08:23:25 +0700

    Time: 3 ms (73.81%), Space: 212.2 MB (45.24%) - LeetHub

--- 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.rb
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.rb
@@ -11,40 +11,20 @@ def min_bitwise_array(nums)
     # 111 -> 100 OR 11
     # 011 -> 001 OR 010
     # 10 -> even number
+    
     nums.map do |num|
-        if num == 3
-            1
-        elsif num == 5
-            4
-        elsif num == 7
-            3
-        elsif num % 8 == 7 # 111
-            if num.to_s(2).count('1') == num.to_s(2).length
-                num / 2
-            else
-                count = 0
+        # even numbers can never be formed by x | (x + 1)
+        next -1 if num.even?
 
-                while num & 1 == 1
-                    num >>= 1
-                    count += 1
-                end
-
-                num <<= 1
-            
-                while count - 1 > 0
-                    num <<= 1
-                    num = num | 1
-
-                    count -= 1
-                end
-
-                num
-            end
-        elsif num % 8 == 3 # 011
-            num - 2
-        elsif num % 4 == 1
-            num - 1
-        else -1
+        # count trailing 1s
+        k = 0
+        temp = num
+        while (temp & 1) == 1
+        k += 1
+        temp >>= 1
         end
+
+        # minimal x
+        num - (1 << (k - 1))
     end
 end
