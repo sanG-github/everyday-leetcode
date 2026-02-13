0335de3d50d3bd17c8a2501cbf2b8e37adfc0213
Sang Huynh Thanh <63148598+sanG-github@users.noreply.github.com>
2026-02-13 09:55:42 +0700

    Time: 89 ms (35.29%), Space: 233 MB (72.06%) - LeetHub

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.rb
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.rb
@@ -1,13 +1,29 @@
 # @param {Integer[][]} matrix
 # @return {Integer}
 def max_matrix_sum(matrix)
-    flat_matrix = matrix.flatten
-    positive_matrix = flat_matrix.map { _1.abs }
-    negative = flat_matrix.select { _1 <= 0 }
+    min_item = Float::INFINITY
+    num_of_negative_items = 0
+    sum = 0
 
-    negative_item = negative.size.even? ? 0 : positive_matrix.sort[0]
+    matrix.each_index do |i|
+        matrix[i].each_index do |j|
+            item = matrix[i][j]
+            min_item = [min_item, item.abs].min
+            sum += item.abs
 
-    puts "negative_item: #{negative_item}"
+            num_of_negative_items += 1 if item <= 0
+        end
+    end
 
-    positive_matrix.sum - 2 * negative_item
+    puts "min_item: #{min_item}, num_of_negative_items: #{num_of_negative_items}"
+
+    num_of_negative_items.even? ? sum : sum - 2 * min_item
+
+    # flat_matrix = matrix.flatten
+    # positive_matrix = flat_matrix.map { _1.abs }
+    # negative = flat_matrix.select { _1 <= 0 }
+
+    # negative_item = negative.size.even? ? 0 : positive_matrix.sort[0]
+
+    # positive_matrix.sum - 2 * negative_item
 end
