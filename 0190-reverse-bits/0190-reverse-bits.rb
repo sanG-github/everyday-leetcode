97194ae78671a54690c7ae1ecc23a4091163288c
Sang Huynh Thanh <63148598+sanG-github@users.noreply.github.com>
2026-02-16 22:51:29 +0700

    Time: 62 ms (47.22%), Space: 211.3 MB (83.33%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.rb
+++ 0190-reverse-bits/0190-reverse-bits.rb
@@ -4,8 +4,10 @@ def reverse_bits(n)
     result = 0
 
     32.times do |i|
+        last_bit = n & 1
+
         result <<= 1
-        result = result |  n & 1
+        result = result | last_bit
 
         n >>= 1
     end
